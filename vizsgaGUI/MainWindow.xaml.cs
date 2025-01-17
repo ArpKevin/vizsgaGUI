b20f3db77dc05d84d2223c21d64da45c268bb9cb
ArpKevin <arpaskevin11@gmail.com>
2025-01-17 12:10:01 +0100

    Add project files.

---
+++ vizsgaGUI/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace vizsgaGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<vizsgazo> vizsgazok = new();
+        string filePath = @"..\..\..\src\adatok.txt";
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            foreach (var item in File.ReadAllLines(filePath))
+            {
+                vizsgazok.Add(new(item));
+            }
+
+            osszesenHanyDiakLabel.Content = $"{vizsgazok.Count()} vizsgázó adatait beolvastuk.";
+
+            tanulokListbox.ItemsSource = vizsgazok.Select(v => v.Nev);
+        }
+
+        private void sikeresVizsgatTettGomb_Click(object sender, RoutedEventArgs e)
+        {
+            sikeresVizsgaLabel.Content = $"{vizsgazok.Count(v => v.erdemjegy(v.Modulok) != "elégtelen")} fő";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            using StreamWriter sw = new StreamWriter(@"..\..\..\src\vizsgaeredmenyek.txt");
+
+            var sikeresVizsgak = vizsgazok.Where(v => v.erdemjegy(v.Modulok) != "elégtelen").Select(v => $"{v.Nev}\t{v.vegeredmeny*100}%\t{v.erdemjegy(v.Modulok)}");
+
+            foreach (var item in sikeresVizsgak)
+            {
+                sw.WriteLine(item);
+            }
+        }
+
+        private void tanuloTextbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            tanuloEredmenye.Content = string.Empty;
+            if (vizsgazok.Exists(v => v.Nev.Contains(tanuloTextbox.Text)))
+            {
+                var keresettTanulo = vizsgazok.First(v => v.Nev.Contains(tanuloTextbox.Text));
+
+                tanuloEredmenye.Content = $"Legjobb eredménye: {keresettTanulo.Modulok.Max()*100}%\nLeggyengébb eredménye: {keresettTanulo.Modulok.Min()*100}%\n{(keresettTanulo.erdemjegy(keresettTanulo.Modulok) == "elégtelen" ? "Sikertelen vizsgát tett" : "Sikeres vizsgát tett" )}";
+            }
+            else
+            {
+                MessageBox.Show("Nem található ilyen vizsgázó");
+            }
+        }
+    }
+}
---
+++ vizsgaGUI/vizsgazo.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vizsgaGUI
+{
+    internal class vizsgazo
+    {
+        public string Nev { get; set; }
+        public double ITesHalozatokIrasbeli { get; set; }
+        public double ProgramozasIrasbeli { get; set; }
+        public double HalozatokA { get; set; }
+        public double HalozatokB { get; set; }
+        public double HalozatokC { get; set; }
+        public double HalozatokD { get; set; }
+        public double SzobeliAngol { get; set; }
+        public double SzobeliIT { get; set; }
+
+        public List<double> Modulok { get; set; }
+
+        public double vegeredmeny {
+            get { return Modulok.Average(); }
+        }
+
+        public string erdemjegy(List<double> Modulok)
+        {
+            if (Modulok.Exists(m => m * 100 < 51))
+            {
+                return "elégtelen";
+            }
+            else
+            {
+                var atlagEredmeny = Modulok.Average() * 100;
+                if (atlagEredmeny < 61)
+                {
+                    return "elégséges";
+                }
+                else if (atlagEredmeny < 71)
+                {
+                    return "közepes";
+                }
+                else if (atlagEredmeny < 81)
+                {
+                    return "jó";
+                }
+                else
+                {
+                    return "jeles";
+                }
+            }
+        }
+
+        public vizsgazo(string sor)
+        {
+            var x = sor.Split(';');
+            Nev = x[0];
+            ITesHalozatokIrasbeli = double.Parse(x[1]);
+            ProgramozasIrasbeli = double.Parse(x[2]);
+            HalozatokA = double.Parse(x[3]);
+            HalozatokB = double.Parse(x[4]);
+            HalozatokC = double.Parse(x[5]);
+            HalozatokD = double.Parse(x[6]);
+            SzobeliAngol = double.Parse(x[7]);
+            SzobeliIT = double.Parse(x[8]);
+            Modulok = new List<double>();
+            for (int i = 1; i < 9; i++)
+            {
+                Modulok.Add(double.Parse(x[i]));
+            }
+        }
+
+        
+    }
+}
